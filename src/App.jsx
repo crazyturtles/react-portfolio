c96fb3a00ddee79a7154fbb7e55b246159ea299a
Rohan Matharu <rohanmatharu@gmail.com>
2025-01-04 16:14:57 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Contact from "./components/Contact";
-import Hero from "./components/Hero";
-import Manual from "./components/Manual";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+import Contact from "./components/contact";
+import Hero from "./components/hero";
+import Manual from "./components/manual";
+import Projects from "./components/projects";
+import Skills from "./components/skills";
 
 const MainLayout = () => (
 	<main className="min-h-screen bg-white">
